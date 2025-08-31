43b5d2189f4ff7bc8c3376d5cb7b60a13e1160b8
Kamil Duvert <kduvert@gmail.com>
2025-08-31 20:08:12 +0200

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Kamil Duvert on 31/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
