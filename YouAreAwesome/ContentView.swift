edcfec6a1facad3180dafc791243ac7b76babbaf
Kamil Duvert <kduvert@gmail.com>
2025-09-02 18:20:00 +0200

    Update after lesson 1.10 .minimumScaleFactor and animations

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -15,25 +15,29 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
-            Spacer()
-            
-            Image(imageName)
-                .resizable()
-                .scaledToFit()
-                .clipShape(RoundedRectangle(cornerRadius: 30))
-                .shadow(radius: 30)
             
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
                 .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+                .animation(.easeInOut(duration: 0.15), value: message)
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
+                .animation(.default, value: imageName)
             
             Spacer()
             
             HStack {
                 Button("Press Me!") {
                     let messages = ["You Are Awesome!",
+                                    "I am so glad that you came to my life and I would like this to be forever",
                                     "You Are Great!",
                                     "Fabulous? That's You!",
                                     "You Are Fantastic!",
